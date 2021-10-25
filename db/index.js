f9f9139eac1a7bedbcf53be5758942457094e16e
Shane D. Tyler <soniccinema@yahoo.com>
2021-10-25 15:01:06 -0700

    first pull request

--- db/index.js
+++ db/index.js
@@ -6,8 +6,58 @@ const client = new Client(DB_URL);
 
 // database methods
 
+async function getProductById(productId) {
+    try {
+        const { rows: [ product ] } = await client.query(`
+      SELECT *
+      FROM product
+      WHERE id=$1;
+    `, [productId]);
+        
+        if (!product) {
+            throw {
+                name: "ProductNotFoundError",
+                message: "Could not find a product with that productId"
+            };
+        }
+        
+        return product;
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function getAllProducts() {
+    try {
+        const { rows } = await client.query(`
+      SELECT *
+      FROM products;
+    `);
+        return rows;
+    } catch (error) {
+        throw error;
+    }
+}
+
+const createProduct = async ({ id, name, description, price, imageURL, inStock, category}) => { 
+
+    try { 
+        const { rows: [ product ] } = await client.query(`
+            INSERT INTO products(id, name, description, price, "imageURL", "inStock", category)
+            VALUES($1, $2, $3, $4, $5, $6, $7)
+            RETURNING *;
+        `, [id, name, description, price, imageURL, inStock, category])
+
+        return product;
+    } catch (error) {
+        throw error;
+    }
+}
 // export
 module.exports = {
   client,
+  getProductById,
+  getAllProducts,
+  createProduct
   // db methods
 }
