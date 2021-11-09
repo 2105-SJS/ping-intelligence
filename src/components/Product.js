255135c57a13e40d210afa2ee17919ed0d1f08f1
Shane D. Tyler <soniccinema@yahoo.com>
2021-11-08 21:15:19 -0800

    fixed errors

--- src/components/Product.js
+++ src/components/Product.js
@@ -13,7 +13,7 @@ const Products = ( { products } ) => {
                   
                 products.map(product => <SingleProduct key={product.id} product={product}/>)
                 
-                products.map( product => /*<SingleProduct key={product.id} product={product} />*/ "placeholder for SingleProduct")
+    
                 }
             </div>
         </>
