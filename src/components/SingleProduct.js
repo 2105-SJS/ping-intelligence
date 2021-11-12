532945b3a78c59eb79f3d4b0171e684a1408687d
Ryan Tran <ryantran9@yahoo.com>
2021-11-11 17:14:25 -0800

    addtocart button & function

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -1,6 +1,33 @@
+import { getContrastRatio } from '@material-ui/core';
 import React from 'react';
+import { callApi } from '../util';
 
-const SingleProduct = ({ product, children }) => {
+const SingleProduct = ({ product, children, addProductToCart, getCart }) => {
+
+    const handleAddtoCart = async (event) => {
+        event.preventDefault();
+        try {
+            if (product && cart) {
+                const productId = Number(product.id)
+                const {id} = cart;
+                if(id) {
+                    const response = await callApi({
+                        url: `orders/${id}/products`,
+                        method: 'POST',
+                        token,
+                        body: {quantity: 1, productId: productId}
+                    })
+                    if (response) {
+                        setMessage(`Dream car was added to the cart!`)
+                        await getCart();
+                        return response;
+                    }
+                }
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
     return product
         ? <div
             style={{ margin: '1.2rem' }}
@@ -15,6 +42,7 @@ const SingleProduct = ({ product, children }) => {
             <div>Image URL: { product.imageUrl }</div>
             <div>In Stock: { product.inStock }</div>
             <div>Category: { product.category }</div>
+            <div><button onClick={handleAddtoCart}>Add To Cart</button></div>
             {
                 children
             }
