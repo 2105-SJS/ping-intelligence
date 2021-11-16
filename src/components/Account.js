f3f53bce22dd38399d5756378aa54bedbed390da
Ryan Tran <ryantran9@yahoo.com>
2021-11-15 17:27:55 -0800

    profile and account style

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,9 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { callApi } from '../util';
 import User from './User';
+import { Typography, TextField, Button, Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    page:{
+        width:'500px',
+        height:'100px',
+        display:'flex',
+        flexFlow:'column',
+        justifyContent:'center',
+        alignItems:'center',
+        color:'black',
+        backgroundColor:'white',
+        borderRadius:'10px',
+        border:'5px solid black',
+        minHeight:'200px',
+        marginLeft:'27rem',
+        marginTop:'15rem'
+    } 
+})
 
 const Account = ( props ) =>
 {
+    const classes = useStyles();
     const token = props.token;
 
     const [ user, setUser ] = useState( {} );
@@ -29,7 +50,12 @@ const Account = ( props ) =>
         } )
     }, [ token ] );
 
-    return <>{ user && user.id ? <User user = { user } ></User>:<>You must be logged in to view this page.</>}</>;
+    return <>
+    <Typography className={classes.page}>
+        { user && user.id ? <User user = { user } ></User>:<>You must be logged in to view this page.</>}
+    </Typography>
+    </>
+    
 }
 
 export default Account;
